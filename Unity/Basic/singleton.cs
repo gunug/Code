413cd8296895aad3ffefa51329b46c9093516e40
GunaJona <gunug@naver.com>
2022-09-08 14:37:05 +0900

    singleton

--- Unity/Basic/singleton.cs
+++ Unity/Basic/singleton.cs
@@ -1,2 +1,24 @@
-.
-test
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonClass : MonoBehaviour
+{
+    
+    //싱글톤 패턴
+    public static SingletonClass instance;
+    //static 정적인 , dynamic 동적인
+    //dynamic : instance = new SingletonClass(); 동적생성
+    //static : SingletonClass.instance
+
+    public Awake(){
+        init();
+    }
+    
+    public SingletonClass init(){
+        if(instance == null){
+            instance = new SingletonClass();
+        }
+        return instance;
+    }
+}
