5526ec7273f5ab9e74c8df30b811de9d3314d70e
GunaJona <gunug@naver.com>
2022-09-12 22:26:12 +0900

    new csharp project

---
+++ CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿  using System.IO;
+  
+  namespace root{
+    public class base_class{
+        static void Main(String[] args) {
+            Console.WriteLine("run!");
+            Sub();
+        }
+        static void Sub(){
+            Console.WriteLine("Hello, World!");
+        }        
+    }
+  }
