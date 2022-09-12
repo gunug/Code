47e561ed579febcb84e909f78694b2d3da706cc4
GunaJona <gunug@naver.com>
2022-09-12 23:10:51 +0900

    add note

--- CSharp/sub_cs/delegate.cs
+++ CSharp/sub_cs/delegate.cs
@@ -3,7 +3,7 @@ namespace root{
     public class delegate_sample{
         public void init(){
             // Instantiate the delegate.
-            Del d1 = DelegateMethod;
+            Del d1 = DelegateMethod; //유형이 맞으면 대리자 등록
             Del d2 = DelegateMethod2;
             Del handler = d1 + d2; //대리자를 덧셈으로 추가 가능
             handler += d2; //가산식 적용가능
@@ -12,13 +12,13 @@ namespace root{
             // Call the delegate.
             handler("delegate sample activate!");
 
-            MethodWithCallback(0,1,handler);
+            MethodWithCallback(0,1,handler); //대리자를 메서드에 전달하여 콜백용으로 사용
 
             int invocationCount = handler.GetInvocationList().GetLength(0); //대리자에 포함된 메서드 숫자 알아내기
             Console.WriteLine("invocationCount:"+invocationCount);
         }
 
-        public delegate void Del(string message);        
+        public delegate void Del(string message); //대리자 선언
 
         public static void DelegateMethod(string message){
             Console.WriteLine(message);
