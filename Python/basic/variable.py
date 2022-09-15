72ca09c717e1628f2ec29923ff640e1a1c0885e8
GunaJona <gunug@naver.com>
2022-09-15 13:20:13 +0900

    python compile setting

---
+++ Python/basic/variable.py
@@ -0,0 +1,18 @@
+#1. Boolean: 참, 거짓
+#2. int: 정수
+#3. float: 실수
+#4. complex: 복소수
+#5. strings: 문자열
+#6. bytes: 바이트
+
+#7. lists
+#8. Tuples
+#9. Sets:
+#10. Dictionaries
+
+x = 1
+y = 2
+x = 3
+
+print(x)
+print(y)
